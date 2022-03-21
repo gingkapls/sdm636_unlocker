a1ff2a849cb6f20efd249809af48f5fb94b040c5
Divyang <divyang_2k10@yahoo.com>
2022-03-22 00:33:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## DATA WILL BE WIPED SO MAKE A BACKUP
+
+1. Clone this repository by running `git clone https://gingkapls/sdm636_unlock` 
+2. cd into `./sdm636_unlock/unlock`
+3. Reboot your phone into fastboot mode by holding down volume up + power on at the same time or alternatively, when the device is powered off.
+4. Connect it to your computer and run `./unlock.sh`.
+5. Done (probably).
+
+> **Note:** If `./unlock.sh` gets stuck without output for more than 30 seconds, then there is a configuration issue. Try connecting plugging the phone into a different usb port, preferably USB 2.0.
+
+#### Credits
+- (liunianliang)[liunianliang@huaqin.com] for the original script.
+
---
+++ unlock.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# set path
+if [ -z "$(command -v fastboot)" ] && [ -e "fastboot" ]; then
+  export PATH=$PWD/bin:$PATH
+fi
+
+# Defaults
+default_platform="sdm636"
+default_buildtype="user"
+default_slot="a" # X00T/D an A only partition device
+
+platform="$(fastboot getvar platform | grep platform | awk '{print $NF}')"
+buildtype="$(fastboot getvar build-type 2>&1 | grep build-type | awk '{print $NF}')"
+slot="$(fastboot getvar current-slot 2>&1 | grep current-slot | awk '{print $NF}')"
+
+secret_key="$(fastboot getvar secret-key-opt 2>&1 | grep secret-key-opt | awk '{print $NF}')"
+secret_partition="$(fastboot oem get_random_partition 2>&1 | grep bootloader | awk '{print $NF}')"
+
+if [ -z "$platform" ];then
+    platform="$default_platform"
+fi
+
+if [ -z "$buildtype" ];then
+    buildtype="$default_buildtype"
+fi
+
+if [ -z "$slot" ];then
+    slot="$default_slot"
+fi
+
+if [ "$buildtype" = "user" ];then
+    printf "%s\n" "$secret_key" > default_key.bin
+    fastboot flash "$secret_partition" default_key.bin
+    fastboot flashing unlock
+    fastboot flashing unlock_critical
+fi
+
+# Download (I don't even know what this part does)
+
+if [ -e "${platform}_$2" ];then
+    printf "begin fastboot download %s\n" "${platform}_$2"
+    fastboot flash $1 ${platform}_$2
+elif [ -e "$2" ];then
+    printf "begin fastboot download %s \n" "$2"
+    fastboot flash $1 $2
+else
+    printf "Can't find file: %s or %s, Skip!\n" "$2" "${platform}_$2"
+fi
+
+read -r answer -p "All done, do you want to reboot(y/n)? "
+if [ "$answer" == "y" ] || [ "$answer" == "Y" ];then
+    fastboot oem recovery_and_reboot
+fi
